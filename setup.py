4115bd058228dbe884de15edcb35063eea817c5a
Greg Krimer <gkrimer@gmail.com>
2015-11-05 09:54:18 -0500

    Adding setup.py file so that PIP can make an egg.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='common',
+      version='0.0.4',
+      author='Unascribed',
+      author_email='tech@polymathventures.co',
+      description='Code intended to be used across Polymath Ventures repositories.',
+      license='BSD',
+      url='https://github.com/PolymathVentures/common',
+      packages=['test'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Topic :: Utilities',
+          'License :: OSI Approved :: BSD License',
+      ],
+      )
