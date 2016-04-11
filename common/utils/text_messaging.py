d0e9eca9c5ae9ce1d7f14824817c1afa99c9efa3
Ralph van Krimpen <ralph.vankrimpen@gmail.com>
2016-04-11 10:25:53 -0500

    Added super basic infobip text messaging util

---
+++ common/utils/text_messaging.py
@@ -0,0 +1,25 @@
+from unidecode import unidecode
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+def send_message(username, password, payload):
+    """
+    Method to send a single message to one or more recipients. It needs the Infobip credentials and a payload
+    according to Infobip's standards: https://dev.infobip.com/docs/fully-featured-textual-message.
+    From value (from, required) could be 'INFOsms' to send messages that can't receive a response.
+    Recipient (to, required) can be a string for single value, or list for multiple values.
+    Message (text, required) will be normalized for compatibility with network providers
+    Returns the decoded Infobip response
+    """
+
+    assert payload.get('from') and payload.get('to') and payload.get('text')
+
+    #Normalize message string for compatibility
+    payload['text'] = unidecode(payload['text'])
+
+    url = "https://api.infobip.com/sms/1/text/single"
+    response = requests.post(url, json=payload, auth=HTTPBasicAuth(username.strip(), password.strip()))
+
+    return response.json()
