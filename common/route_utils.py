60f34a3c0e48b4236151f635a40aa6355d2cdbcf
Greg Krimer <gkrimer@gmail.com>
2016-04-24 17:32:36 -0500

    Oops.

--- common/route_utils.py
+++ common/route_utils.py
@@ -11,10 +11,10 @@ def nocache(view):
     @wraps(view)
     def no_cache_wrapper(*args, **kwargs):
         response = make_response(view(*args, **kwargs))
-        # response.headers['Last-Modified'] = datetime.now()
-        # response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
-        # response.headers['Pragma'] = 'no-cache'
-        # response.headers['Expires'] = '-1'
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
         return response
 
     return no_cache_wrapper
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='common',
-      version='0.1.6',
+      version='0.1.7',
       author='Unascribed',
       author_email='tech@polymathventures.co',
       description='Code intended to be used across Polymath Ventures repositories.',
